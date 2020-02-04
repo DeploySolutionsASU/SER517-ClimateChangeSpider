5d41adad7c9e809efaec48e1aa2fef85e3bedbd6
AnkitaShivanand <ankitashivanand25@gmail.com>
2020-02-04 12:38:50 -0700

    Write a script to unzip the downloaded files

--- src/MainScraper.py
+++ src/MainScraper.py
@@ -1,6 +1,7 @@
 import os
 import urllib.request
 import wget
+import gzip
 
 from time import time
 from bs4 import BeautifulSoup
@@ -9,6 +10,7 @@ from bs4 import BeautifulSoup
 root_dir = os.path.abspath('..')
 root_dir = root_dir.replace('\\', '/')
 output_path = root_dir + '/files'
+extraction_path = root_dir + '/data'
 
 
 def download_file(url):
@@ -16,6 +18,18 @@ def download_file(url):
         wget.download(url, out=output_path)
     except Exception as e:
         print(e)
+
+
+def unzip_files(eachfile, input_file):
+    curr_path = root_dir +'/data'
+    file_name_output = input_file.split('.gz')[0] + '.txt'
+    output_path = curr_path + '/' +file_name_output
+    fp = open(output_path, "wb")
+    with gzip.open(eachfile, "rb") as f:
+        bindata = f.read()
+    fp.write(bindata)
+    fp.close()
+
 
 if __name__ == '__main__':
 
@@ -61,5 +75,12 @@ if __name__ == '__main__':
         download_file(link)
 
     print("Time to download", time() - start)
+    for filename in os.listdir(output_path):
+        if filename.endswith(".gz"):
+            file_name_input = filename
+            each_file = os.path.join(output_path, filename)
+            unzip_files(each_file, file_name_input)
+        else:
+            continue
 
 
