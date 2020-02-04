2824a035cd9d78079fd1d83b7b98974dc34e7563
AnkitaShivanand <ankitashivanand25@gmail.com>
2020-02-04 16:09:42 -0700

    Write a script to unzip the downloaded files

--- src/MainScraper.py
+++ src/MainScraper.py
@@ -33,48 +33,48 @@ def unzip_files(eachfile, input_file):
 
 if __name__ == '__main__':
 
-    # given_url = 'http://webdatacommons.org/structureddata/2019-12/stats/how_to_get_the_data.html'
-    # search_text = 'rdfa'
-    #
-    # page = urllib.request.urlopen(given_url)
-    # soup = BeautifulSoup(page, 'html.parser')
-    #
-    #
-    # down_dir = root_dir + '/downloads'
-    #
-    # output_url = set()
-    # for a in soup.findAll('a', href=True):
-    #     if search_text in a.text:
-    #         index = a.get('href').index('/')
-    #         trim_url = 'http://webdatacommons.org/structureddata/2019-12' + a.get('href')[index:]
-    #         output_url.add(trim_url)
-    #
-    # file_path = os.path.join(down_dir, 'urls.txt')
-    # for out_url in output_url:
-    #     urllib.request.urlretrieve(out_url, file_path)
-    #
-    # fileHandler = open(file_path, "r")
-    # Lines = fileHandler.readlines()
-    # fileHandler.close()
-    #
-    # url_list = set()
-    # count = 0
-    # for line in Lines:
-    #     # Testing download functionality with only 5 URLs
-    #     #url_list.append(line.strip())
-    #     if count == 5:
-    #         break
-    #     else:
-    #         url_list.add(line.strip())
-    #         count = count + 1
-    #
-    #
-    # start = time()
-    #
-    # for link in url_list:
-    #     download_file(link)
-    #
-    # print("Time to download", time() - start)
+    given_url = 'http://webdatacommons.org/structureddata/2019-12/stats/how_to_get_the_data.html'
+    search_text = 'rdfa'
+
+    page = urllib.request.urlopen(given_url)
+    soup = BeautifulSoup(page, 'html.parser')
+
+
+    down_dir = root_dir + '/downloads'
+
+    output_url = set()
+    for a in soup.findAll('a', href=True):
+        if search_text in a.text:
+            index = a.get('href').index('/')
+            trim_url = 'http://webdatacommons.org/structureddata/2019-12' + a.get('href')[index:]
+            output_url.add(trim_url)
+
+    file_path = os.path.join(down_dir, 'urls.txt')
+    for out_url in output_url:
+        urllib.request.urlretrieve(out_url, file_path)
+
+    fileHandler = open(file_path, "r")
+    Lines = fileHandler.readlines()
+    fileHandler.close()
+
+    url_list = set()
+    count = 0
+    for line in Lines:
+        # Testing download functionality with only 5 URLs
+        #url_list.append(line.strip())
+        if count == 5:
+            break
+        else:
+            url_list.add(line.strip())
+            count = count + 1
+
+
+    start = time()
+
+    for link in url_list:
+        download_file(link)
+
+    print("Time to download", time() - start)
 
     # Read the files from the directory and unzip
     for filename in os.listdir(output_path):
