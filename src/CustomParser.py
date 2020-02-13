6da8392ed73d3ed69b737a425f5be7447e4f58bc
yuvanpradeep <yuvanpradeep594@gmail.com>
2020-02-13 00:17:05 -0700

    US#8-Task#9 Mock the run command to test the error removal function

--- src/CustomParser.py
+++ src/CustomParser.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 
 file_path = "C:/Yuvan/Studies/Fourth_sem/ser517/apache-jena-3.14.0/bin/riot --validate "
-
 root_dir = os.path.abspath('..')
 root_dir = root_dir.replace('\\', '/')
 output_path = root_dir + '/data/'
@@ -11,24 +10,29 @@ output_path = root_dir + '/data/'
 def parse_file(file_name):
     command = (file_path + output_path + file_name).split()
     filePath = output_path + file_name
-    output = run_command(command)
+    # output = run_command(command)
 
-    if 'Error' in output:
-        #get the line number of the errors
-        print("line number")
-    else:
+    #Mocking the run_command function
+    error_line_nos = []
+    with open(filePath, "r") as input:
+        for lineno, line in enumerate(input):
+            if 'Error' in line:
+                error_line_nos.append(lineno)
+                break
+
+    if len(error_line_nos) == 0:
         return
 
+    with open(filePath, "r") as f:
+        lines = f.readlines()
 
-    with open(filePath, "r") as input:
-        with open(filePath, "w") as output:
-            for lineno, line in enumerate(input):
-                if lineno != "Error line number": #if not error line number write into file
-                    output.write(line)
+    with open(filePath, "w") as f:
+        for lineno, line in enumerate(lines):
+            if lineno not in error_line_nos: #if not error line number write into file
+                f.write(line)
 
     parse_file(file_name)
-
-    print(output)
+    print("Parsing done")
 
 
 def run_command(command):
