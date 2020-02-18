74cbe4ae2da92813c735390b84b07ff327206f14
AnkitaShivanand <ankitashivanand25@gmail.com>
2020-02-17 22:40:23 -0700

    Task#42 Modularize the code for data extraction

--- src/Config.py
+++ src/Config.py
@@ -7,5 +7,12 @@ global_config = {
 path_config = {
     "site_url": "http://webdatacommons.org/structureddata/2019-12/stats/how_to_get_the_data.html",
     "fuseki_url": "http://apache.mirrors.lucidnetworks.net/jena/binaries/apache-jena-fuseki-3.14.0.zip",
-    "data_source_url": "http://webdatacommons.org/structureddata/2019-12"
+    "data_source_url": "http://webdatacommons.org/structureddata/2019-12",
+    "server_name": "apache-jena-fuseki-3.14.0.zip",
+    "extract_folder": "data"
+}
+
+data_formats_config = {
+    "search_text": ['rdfa', 'microdata', 'embedded', 'mf-hcard']
+
 }
--- src/MainScraper.py
+++ src/MainScraper.py
@@ -2,47 +2,36 @@ import os
 import urllib.request
 import gzip
 from zipfile import ZipFile
-from Config import path_config
+from Config import path_config, data_formats_config
 
 from time import time
 from bs4 import BeautifulSoup
 
 from DownloadManager import download_file
+from FileUtilities import unzip_files
 from HelperManager import create_directory
 from HelperManager import get_root_directory
 
-
 output_path = get_root_directory() + '/downloads'
-fuseki_path = get_root_directory + '/fuseki'
-
-def unzip_files(eachfile, input_file):
-    curr_path = get_root_directory +'/data'
-    file_name_output = input_file.split('.gz')[0] #+ '.txt'
-    output_path = curr_path + '/' +file_name_output
-    fp = open(output_path, "wb")
-    with gzip.open(eachfile, "rb") as f:
-        bindata = f.read()
-    fp.write(bindata)
-    fp.close()
-
+server_path = get_root_directory() + '/fuseki'
 
 if __name__ == '__main__':
 
-    mainFile_path = os.path.join(get_root_directory, 'mainFile')
-    downloads_path = os.path.join(get_root_directory, 'downloads')
-    fuseki_dir_path = os.path.join(get_root_directory, 'fuseki')
+    mainFile_path = os.path.join(get_root_directory(), 'mainFile')
+    downloads_path = os.path.join(get_root_directory(), 'downloads')
+    fuseki_dir_path = os.path.join(get_root_directory(), 'fuseki')
 
     create_directory(mainFile_path)
     create_directory(downloads_path)
     create_directory(fuseki_dir_path)
 
     given_url = path_config["site_url"]
-    search_text = ['rdfa', 'microdata', 'embedded', 'mf-hcard']
+    search_text = data_formats_config["search_text"]
     fuseki_url = path_config["fuseki_url"]
 
     page = urllib.request.urlopen(given_url)
     soup = BeautifulSoup(page, 'html.parser')
-    down_dir = get_root_directory + '/mainFile'
+    down_dir = get_root_directory() + '/mainFile'
 
     output_url = set()
     for a in soup.findAll('a', href=True):
@@ -81,20 +70,22 @@ if __name__ == '__main__':
 
     print("Time to download", time() - start)
 
-    extraction_path = os.path.join(get_root_directory, 'data')
+    extraction_path = os.path.join(get_root_directory(), path_config["extract_folder"])
     create_directory(extraction_path)
 
     # Read the files from the directory and unzip
     for filename in os.listdir(output_path):
         if filename.endswith(".gz"):
-            file_name_input = filename
-            each_file = os.path.join(output_path, filename)
-            unzip_files(each_file, file_name_input)
+            file = os.path.join(output_path, filename)
+            curr_path = get_root_directory() + "/" + path_config["extract_folder"]
+            file_name_output = filename.split('.gz')[0]
+            output_path = curr_path + '/' + file_name_output
+            unzip_files(file, output_path)
         else:
             continue
 
-    zip_file = os.path.join(fuseki_path, 'apache-jena-fuseki-3.14.0.zip')
-    download_file(fuseki_url, fuseki_path)
+    zip_file = os.path.join(server_path, path_config["server_name"])
+    download_file(fuseki_url, server_path)
     with ZipFile(zip_file, 'r') as zipObj:
         # Extract all the contents of zip file in current directory
-        zipObj.extractall(fuseki_path)
+        zipObj.extractall(server_path)
