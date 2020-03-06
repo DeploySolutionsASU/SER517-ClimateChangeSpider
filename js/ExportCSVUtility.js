702d00f05e5e6d9918f5280678c8f9d8e7c9d289
AnkitaShivanand <ankitashivanand25@gmail.com>
2020-03-05 22:33:36 -0700

    US#48-Task#55 Export search results CSV - export method

--- js/ExportCSVUtility.js
+++ js/ExportCSVUtility.js
@@ -45,8 +45,6 @@ function getHeaders(jsonObject) {
 
 }
 
-
-
 //Parsing of the response
 function getItems(headers, response) {
 	rows = []
@@ -69,3 +67,36 @@ function getItems(headers, response) {
 
   }
 }
+
+
+function exportCSVFile(headers, items, fileTitle) {
+    if (headers) {
+        items.unshift(headers);
+    }
+
+    // Convert Object to JSON
+    var jsonObject = JSON.stringify(items);
+
+    var csv = this.convertToCSV(jsonObject);
+
+    var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+
+    var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, exportedFilenmae);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", exportedFilenmae);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
+
+
