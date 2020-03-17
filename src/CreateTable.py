df873375830b6c93e3613a2eef860e064bfdb881
Narendramohanm <nmuralim@asu.edu>
2020-03-17 11:41:55 -0700

    US#56-Task#67 handling errors

--- src/CreateTable.py
+++ src/CreateTable.py
@@ -1,49 +1,57 @@
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.client('dynamodb')
 
-primaryTable = dynamodb.create_table(
-    TableName='primaryTable',
-    KeySchema=[
-        {
-            'AttributeName': 'job_id',
-            'KeyType': 'HASH'
+try:
+    primaryTable = dynamodb.create_table(
+        TableName='primaryTable',
+        KeySchema=[
+            {
+                'AttributeName': 'job_id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'job_id',
+                'AttributeType': 'N'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
         }
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'job_id',
-            'AttributeType': 'N'
+    )
+
+    primaryTable.meta.client.get_waiter('table_exists').wait(TableName='primaryTable')
+
+except dynamodb.exceptions.ResourceInUseException:
+    print("Primary table already exists")
+
+try:
+    secondaryTable = dynamodb.create_table(
+        TableName='secondaryTable',
+        KeySchema=[
+            {
+                'AttributeName': 'partition_id',
+                'KeyType': 'HASH'
+            },
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'partition_id',
+                'AttributeType': 'N'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
         }
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
+    )
 
-primaryTable.meta.client.get_waiter('table_exists').wait(TableName='primaryTable')
+    secondaryTable.meta.client.get_waiter('table_exists').wait(TableName='secondaryTable')
 
-secondaryTable = dynamodb.create_table(
-    TableName='secondaryTable',
-    KeySchema=[
-        {
-            'AttributeName': 'partition_id',
-            'KeyType': 'HASH'
-        },
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'partition_id',
-            'AttributeType': 'N'
-        }
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
-
-secondaryTable.meta.client.get_waiter('table_exists').wait(TableName='secondaryTable')
+except dynamodb.exceptions.ResourceInUseException:
+    print("Secondary table already exists")
 
 
