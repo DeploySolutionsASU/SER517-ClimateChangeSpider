5c9dcd2709fbb5c170bcb97f2542dea13a077916
yuvanpradeep <yuvanpradeep594@gmail.com>
2020-03-21 23:11:51 -0700

    US#56-Task#71 updated the parser and added run command

--- AWS/ParserLambda.py
+++ AWS/ParserLambda.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 import boto3
 import re
 from subprocess import call
@@ -18,25 +19,10 @@ def lambda_handler(event, context):
     final_content = record["Body"].read().splitlines()
     if not os.path.isdir('/tmp/fuseki'):
         shutil.copytree('/var/task/fuseki', '/tmp/fuseki')
-        print('Folder copied')
-
-    if not os.path.isdir('/tmp/fuseki/apache-jena-3.14.0/bin'):
-        print('not exits')
-    else:
-        print('exists')
 
     riot_path = '/tmp/fuseki/apache-jena-3.14.0/bin/riot'
-    riot_path1 = '/tmp/fuseki/apache-jena-3.14.0/bin'
-    riot_path2 = '/tmp/fuseki/apache-jena-3.14.0'
-    # command = "export" + " " + "PATH" + "=" + "/tmp/fuseki/apache-jena-3.14.0/bin"
-    # set_environment(command)
     os.chmod(riot_path, 0o777)
-    os.chmod(riot_path1, 0o777)
-    os.chmod(riot_path2, 0o777)
-    mask = oct(os.stat(riot_path).st_mode)[-3:]
-    print("File permission--", mask)
     parse_file(s3object, final_content)
-
 
 
 def parse_file(filename, data):
@@ -50,14 +36,20 @@ def parse_file(filename, data):
             for line in data:
                 file.write('%s\n' % line.decode('utf-8'))
 
-
         error_line_nos = set()
-
+        validation_result = run_command(command_template.format(file_path=output_file_path).split())
+        for result in validation_result:
+            result = str(result)
+            splits = result.split("::")
+            print('result----', result)
+            print('splits[0]----', splits[0])
+            if re.search('ERROR riot', splits[0]):
+                error_line_nos.add(int(re.findall(r'[0-9]+', splits[1])[0]))
         if len(error_line_nos) == 0:
             news3 = boto3.client('s3')
             file_path = '/tmp/' + filename
             news3.upload_file(file_path, 's3parsedfiles',
-                                         filename)
+                              filename)
             print('Upload success!!')
             return True
 
@@ -72,3 +64,11 @@ def parse_file(filename, data):
         return False
     else:
         print(filename, "done")
+
+
+def run_command(command):
+    print('command---', command)
+    file_output = subprocess.Popen(command,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+    return iter(file_output.stdout.readline, b'')
