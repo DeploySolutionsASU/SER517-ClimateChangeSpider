347dcfbdcd2d9d86df6dafa2cb4ee39fe39d5e72
Narendramohanm <nmuralim@asu.edu>
2020-04-22 22:56:32 -0700

    US#114-Task#115 Deployed query executor in EC2

--- src/ElasticSearchManager.py
+++ src/ElasticSearchManager.py
@@ -12,7 +12,8 @@ def create_bulk_index(index_name, file_path):
     # Json file path
     data = open(file_path, 'rb').read()
     # Elastic search URL
-    url = 'http://localhost:9200/' + index_name + '/_doc/_bulk'
+    url = 'https://search-cc14-prototype-s5q5rjhkogrxzrmfzutzt4umnm.ca-central-1.es.amazonaws.com' + index_name + '/_doc/_bulk'  
+    print(index_name)
     response = requests.post(url, headers=headers, params=params, data=data)
 
     print(response)
