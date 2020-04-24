ff0f4ef7f73ea3d7d4f9829fc7978c9c6373e1df
Narendramohanm <nmuralim@asu.edu>
2020-04-23 21:12:07 -0700

    US#114-Task#115 updated the AWS elasticsearch link

--- src/ElasticSearchManager.py
+++ src/ElasticSearchManager.py
@@ -12,7 +12,7 @@ def create_bulk_index(index_name, file_path):
     # Json file path
     data = open(file_path, 'rb').read()
     # Elastic search URL
-    url = 'https://search-cc14-prototype-s5q5rjhkogrxzrmfzutzt4umnm.ca-central-1.es.amazonaws.com' + index_name + '/_doc/_bulk'  
+    url = 'https://search-cc14-prototype-s5q5rjhkogrxzrmfzutzt4umnm.ca-central-1.es.amazonaws.com/' + index_name + '/_doc/_bulk'  
     print(index_name)
     response = requests.post(url, headers=headers, params=params, data=data)
 
